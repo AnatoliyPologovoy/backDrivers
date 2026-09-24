14c0c200a2534cc7dded4e6b4dc0e69d2b5c4d74
AnatoliiPologovoy <pologovoy89@mail.ru>
2026-09-24 07:04:02 +0300

    правки по валидации  trim: true,

--- src/blogs/validation/blog.validation.ts
+++ src/blogs/validation/blog.validation.ts
@@ -10,8 +10,16 @@ const URL_REGEX =
   /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/;
 export const inputBlogBodyValidation = checkSchema(
   {
-    name: { exists: true, isLength: { options: { min: 1, max: 15 } } },
-    description: { exists: true, isLength: { options: { min: 1, max: 500 } } },
+    name: {
+      trim: true,
+      exists: true,
+      isLength: { options: { min: 1, max: 15 } },
+    },
+    description: {
+      trim: true,
+      exists: true,
+      isLength: { options: { min: 1, max: 500 } },
+    },
     websiteUrl: {
       trim: true,
       exists: true,
